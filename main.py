9e79930712af6e37bba5b1843c576289b9de2173
Sami-M-H-Aden <adens7066@gmail.com>
2024-07-22 15:34:03 +0100

    made a function to read the books

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+def main():
+    file_contents = get_book_contents("frankenstein")
+    print(file_contents)
+def get_book_contents(book_name):
+    with open(f"books/{book_name}.txt") as f:
+        file_contents = f.read()
+    return file_contents
+
+
+main()
