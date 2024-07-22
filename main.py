326483e0e54fea27b6ec934ba0058a315a580cef
Sami-M-H-Aden <adens7066@gmail.com>
2024-07-22 19:08:33 +0100

    Made the book report feature

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 def main():
     file_contents = get_book_contents("frankenstein")
-    num_words = word_counter(file_contents)
-    print(f"{num_words} words were found in the document")
-    print(count_characters(file_contents))
+    report_maker(file_contents)
 
 def get_book_contents(book_name):
     with open(f"books/{book_name}.txt") as f:
@@ -23,5 +21,16 @@ def count_characters(file_contents):
             char_dict[i] = 1
     return char_dict
 
+def listing_part_of_report(file_contents):
+    char_dict = count_characters(file_contents)
+    for i in char_dict:
+        if i.isalpha():
+            print(f"The '{i}' character was found {char_dict[i]} times ")
 
+
+def report_maker(file_contents):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_counter(file_contents)} words found in the document \n")
+    listing_part_of_report(file_contents)
+    print("--- End report ---")
 main()
