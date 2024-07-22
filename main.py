ca8bfa09e7dd027f26b325f1eff71108400195be
Sami-M-H-Aden <adens7066@gmail.com>
2024-07-22 16:24:21 +0100

    making character counter

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     file_contents = get_book_contents("frankenstein")
     num_words = word_counter(file_contents)
     print(f"{num_words} words were found in the document")
+    print(count_characters(file_contents))
 
 def get_book_contents(book_name):
     with open(f"books/{book_name}.txt") as f:
@@ -11,5 +12,18 @@ def get_book_contents(book_name):
 def word_counter(file_contents):
     words = file_contents.split()
     return len(words)
+
+def count_characters(file_contents):
+    char_dict = {}
+    lowered_contents = file_contents.lower()
+    lowered_contents = lowered_contents.split()
+    for i in lowered_contents:
+        for j in i:
+            if j in char_dict:
+                char_dict[j] += 1
+            else:
+                char_dict[j] = 1
+    return char_dict
+
 
 main()
